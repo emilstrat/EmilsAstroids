dad5714188d074dfd8643b92032162d6cb5479ad
emilstrat <emil.strat@gmail.com>
2024-11-20 19:51:10 +0100

    Main menu

--- hud.gd
+++ hud.gd
@@ -2,15 +2,6 @@ extends CanvasLayer
 
 signal start_game
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func show_message(text):
 	$Message.text = text
 	$Message.show()
@@ -19,10 +10,8 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	await $MessageTimer.timeout
-	
 	$Message.text = "Dodge the Creeps!"
 	$Message.show()
-	
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 	
@@ -31,7 +20,6 @@ func update_score(score):
 	
 func _on_start_button_pressed():
 	$StartButton.hide()
-	$Message.text = "Dodge the Creeps!!"
 	start_game.emit()
 	
 func _on_message_timer_timeout():
