23fa642811b4d302f1df4d81e8bb88e4dd36e804
lindforsjohan <19lindfors@gmail.com>
2024-11-20 19:51:12 +0100

    Main menu

--- main.gd
+++ main.gd
@@ -3,23 +3,17 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready() -> void:
-	new_game()
-
-
-func _process(delta: float) -> void:
-	pass
-
-
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -39,10 +33,9 @@ func _on_mob_timer_timeout() -> void:
 	
 	add_child(mob)
 
-
 func _on_score_timer_timeout() -> void:
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
--- player.gd
+++ player.gd
@@ -4,13 +4,10 @@ signal hit
 @export var speed = 400
 var screen_size
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
